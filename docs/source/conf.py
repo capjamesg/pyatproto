b8c9b00a4a6646be42117b67eb581396de032c1a
capjamesg <jamesg@jamesg.blog>
2023-05-04 01:41:40 +0100

    add new docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,21 +11,34 @@ import os
 project = "pyatproto"
 copyright = "2023, capjamesg"
 author = "capjamesg"
-release = "0.0.1"
+release = "0.1.0"
 
-os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.insert(0, os.path.abspath("../../src/"))
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = "0.8.0"
+version = "0.8.0"
 
-extensions = []
+# -- General configuration
+
+extensions = [
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
-exclude_patterns = []
 
+# -- Options for HTML output
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Options for EPUB output
+epub_show_urls = "footnote"
 
-html_theme = "alabaster"
 html_static_path = ["_static"]

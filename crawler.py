f81aa5507ac0cab6dd07d47a64ba59b7e5ad4133
capjamesg <jamesg@jamesg.blog>
2023-05-04 06:32:22 +0100

    update crawler

--- crawler.py
+++ crawler.py
@@ -30,9 +30,9 @@ def get_followers(user):
     return followers
 
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
     future_to_url = {executor.submit(get_followers, USERNAME): USERNAME}
-    while future_to_url and len(users) < 2000:
+    while future_to_url and len(users) < 10000:
         done, _ = concurrent.futures.wait(
             future_to_url, return_when=concurrent.futures.FIRST_COMPLETED
         )
@@ -48,6 +48,9 @@ all_posts = {}
 
 print("USERS: " + str(len(users)))
 
+# save users to file
+with open("users.json", "w") as f:
+    json.dump(list(users), f)
 
 def get_user_feed(user):
     print("USER: " + user)
@@ -60,18 +63,21 @@ def get_user_feed(user):
     return all_posts
 
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
     future_to_url = {executor.submit(get_user_feed, user): user for user in users}
     while future_to_url:
-        done, _ = concurrent.futures.wait(
-            future_to_url, return_when=concurrent.futures.FIRST_COMPLETED
-        )
-        for future in done:
-            del future_to_url[future]
-            user = future.result()[0]
-            all_posts.update(user)
-            print("POSTS: " + str(len(all_posts)))
-            print("QUEUE: " + str(len(future_to_url)))
+        try:
+            done, _ = concurrent.futures.wait(
+                future_to_url, return_when=concurrent.futures.FIRST_COMPLETED
+            )
+            for future in done:
+                del future_to_url[future]
+                user = future.result()
+                all_posts.update(user)
+                print("POSTS: " + str(len(all_posts)))
+                print("QUEUE: " + str(len(future_to_url)))
+        except:
+            continue
 
 with open("posts.json", "w") as f:
     json.dump(all_posts, f)
